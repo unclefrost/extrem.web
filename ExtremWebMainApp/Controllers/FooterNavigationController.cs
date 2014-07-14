2115b796d478d1be0380b63388f2e3c55b7bf477
Nazarii Tashak <nazarii.tashak@ericpol.com>
2014-07-14 12:01:08 +0300

    Navigation bar added

---
+++ ExtremWebMainApp/Controllers/FooterNavigationController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExtremWebMainApp.Controllers
+{
+    public class FooterNavigationController : Controller
+    {
+        //
+        // GET: /FooterNavigation/
+
+        public ActionResult ClubLeadership()
+        {
+            return View();
+        }
+
+        public ActionResult OurCoordinates()
+        {
+            return View();
+        }
+
+        public ActionResult Achievements()
+        {
+            return View();
+        }
+
+        public ActionResult ClubStatus()
+        {
+            return View();
+        }
+
+        public ActionResult LeadershipDecision()
+        {
+            return View();
+        }
+
+    }
+}
